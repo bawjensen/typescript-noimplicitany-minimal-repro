b62e036a07fd1dd7c9144d6364eeeacdf41a4715
Bryan Jensen <bryan@thumbtack.com>
2020-04-29 11:28:24 -0700

    Make the minimal repro more minimal by removing JS code

--- two.ts
+++ two.ts
@@ -1,5 +1,10 @@
-import { fetchUrlAsJson as fetchUrlAsJsonOriginal } from './three';
+function checkResponse(response: Response): Response {
+    if (!response.ok) {
+        throw new Error("Response wasn't ok.");
+    }
+    return response;
+}
 
 export function fetchUrlAsJson<T>(url: string): Promise<T> {
-    return fetchUrlAsJsonOriginal(url);
+    return fetch(url).then(checkResponse).then((response) => response.json());
 }
